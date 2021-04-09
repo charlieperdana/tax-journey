9cf0b11d1fc4540ecbc85e4caff97bd1da246b60
charlieperdana <charlie.siagian@gmail.com>
2021-04-09 14:05:30 +0700

    page penghasilan per tahun

---
+++ TaxJourney/ListPenghasilanItemModel.swift
@@ -0,0 +1,18 @@
+//
+//  ListPenghasilanItemModel.swift
+//  TaxJourney
+//
+//  Created by charlie siagian on 07/04/21.
+//
+
+import Foundation
+
+struct ListPenghasilanItemModel {
+    var item : String = ""
+    var detailInfo : String = ""
+}
+
+struct JumlahPotongModel {
+    var item : String = ""
+    var jumlah : Int = 0
+}
