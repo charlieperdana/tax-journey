1b2a4ea05d57cebfb8757eb3978a2f55321efc6c
abayhub <81407855+abayhub@users.noreply.github.com>
2021-04-13 21:11:21 +0700

    Revert "Akbar/page baru banget"

--- TaxJourney/ViewController.swift
+++ TaxJourney/ViewController.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var haiLabel: UILabel!
+
     @IBOutlet weak var btnStart: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.isNavigationBarHidden = true
+        
         initDisplay()
     }
     
@@ -22,9 +22,7 @@ class ViewController: UIViewController {
         btnStart.layer.cornerRadius = 10
         btnStart.clipsToBounds = true
     }
-    
-    @IBAction func startBtnDipTap(_ sender: Any) {
-        performSegue(withIdentifier: "toPenghasilanPerTahun", sender: self)
-    }
+
+
 }
 
