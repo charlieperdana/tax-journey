8168a55d534d93835ed6318220323d98840f0149
scaltielgloriaa <65727457+scaltielgloriaa@users.noreply.github.com>
2021-04-13 21:54:33 +0700

    Rincian PTKP

--- TaxJourney/RincianPTKPViewController.swift
+++ TaxJourney/RincianPTKPViewController.swift
@@ -41,15 +41,7 @@ class RincianPTKPViewController: UIViewController {
         @IBAction func infoButtonTotal(_ sender: UIButton) {
         miniView2()
     }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 extension RincianPTKPViewController: UIViewControllerTransitioningDelegate{
